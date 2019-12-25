c45f63c6cc9e625c49b3a2f2a6455b0ceecaac44
SirotenkoMax <sirotenko.max1998@gmail.com>
2019-12-25 23:46:08 +0300

    Added Hand Poses And Colliders

---
+++ Assets/Scripts/DoorUnlock.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorUnlock : MonoBehaviour
+{
+    JointLimits jointLimits;
+    [SerializeField] FixedJoint fixedJoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        jointLimits = GetComponent<HingeJoint>().limits;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.eulerAngles.y == jointLimits.min || transform.eulerAngles.y == jointLimits.max)
+        {
+            Destroy(fixedJoint);
+        }
+    }
+}
