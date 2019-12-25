d95217ccf5f7ad5d589a17546d646cedc6e7b124
SirotenkoMax <sirotenko.max1998@gmail.com>
2019-12-26 00:00:30 +0300

    Fixed Open Door Script

--- Assets/Scripts/DoorUnlock.cs
+++ Assets/Scripts/DoorUnlock.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 public class DoorUnlock : MonoBehaviour
 {
     JointLimits jointLimits;
+    HingeJoint hinge;
     [SerializeField] FixedJoint fixedJoint;
 
     // Start is called before the first frame update
     void Start()
     {
-        jointLimits = GetComponent<HingeJoint>().limits;
+        hinge = GetComponent<HingeJoint>();
+        jointLimits = hinge.limits;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.eulerAngles.y == jointLimits.min || transform.eulerAngles.y == jointLimits.max)
+        //if(hinge.angle <= jointLimits.min || hinge.angle >= jointLimits.max)
+        //{
+         //   Destroy(fixedJoint);
+        //}
+        if (Mathf.Abs(hinge.angle - jointLimits.min) < 1 || Mathf.Abs(hinge.angle - jointLimits.max) < 1)
         {
             Destroy(fixedJoint);
         }
