88821665c9ab61f9d9f40a73530ca0a4432095b2
SirotenkoMax <sirotenko.max1998@gmail.com>
2020-02-17 20:14:35 +0300

    Glow When Grab Added

---
+++ Assets/Scripts/GlowNearGrab.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class GlowNearGrab : MonoBehaviour
+{
+    [SerializeField] Material GlowMaterial;
+    Material StartMaterial;
+
+    private void Start()
+    {
+        StartMaterial = GetComponent<Renderer>().material;
+    }
+
+    public void GlowControll(bool isGlowing)
+    {
+        if (isGlowing)
+        {
+            Material material = new Material(GlowMaterial);
+            material.SetTexture("_Albedo", StartMaterial.GetTexture("_BaseColorMap"));
+            //material.SetTexture("_Metallic", StartMaterial.GetTexture("_Metallic"));
+            material.SetTexture("_Normal", StartMaterial.GetTexture("_NormalMap"));
+            GetComponent<MeshRenderer>().material = material;
+
+        }
+        else
+        {
+            GetComponent<Renderer>().material = StartMaterial;
+        }
+    }
+
+}
